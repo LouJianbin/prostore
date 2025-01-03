6e791989a8cbdac171fce29a10a8564a18a06b3c
Uncle <1215661954@qq.com>
2025-01-04 00:17:57 +0800

    10 -Header & Footer Components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import Footer from "@/components/footer";
+import Header from "@/components/shared/header";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <div className="flex h-screen flex-col">
-      <main>{children}</main>
+      <Header></Header>
+      <main className="flex-1 wrapper">{children}</main>
+      <Footer></Footer>
     </div>
   );
 }
