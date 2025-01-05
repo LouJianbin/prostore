474621c268af127b60891e9ce88c9d7417407ed5
Uncle <1215661954@qq.com>
2025-01-05 15:57:09 +0800

    12 -Loading & Not Found Pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,7 @@
-
-export default function Home() {
-  return <><span className=" text-orange-500">Prostore</span></>;
+export default async function Home() {
+  return (
+    <>
+      <span className=" text-orange-500">Prostore</span>
+    </>
+  );
 }
