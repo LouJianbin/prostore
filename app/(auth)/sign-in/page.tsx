c6fe81fbcb05ef18c3d57fd49bfcca72e90a322d
Uncle <1215661954@qq.com>
2025-01-08 01:38:41 +0800

    34 -Credentials Sign In Form

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import CredentialsSignInForm from "./credentials-signin-form";
+
+const SignInPage = () => {
+  return (
+    <div>
+      <CredentialsSignInForm></CredentialsSignInForm>
+    </div>
+  );
+};
+
+export default SignInPage;
