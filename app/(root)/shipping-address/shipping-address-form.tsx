211661967bb30c284b486a040585e6fd04f3c97d
Uncle <1215661954@qq.com>
2025-01-13 02:15:14 +0800

    57 -Shipping Address & Form

---
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import { useToast } from "@/hooks/use-toast";
+import { ShippingAddress } from "@/types";
+import { useRouter } from "next/navigation";
+import { ControllerRenderProps, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { shippingAddressSchema } from "@/lib/validators";
+import { shippingAddressDefaultValues } from "@/lib/constants";
+import { useTransition } from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader } from "lucide-react";
+
+const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const form = useForm<ShippingAddress>({
+    resolver: zodResolver(shippingAddressSchema),
+    defaultValues: address || shippingAddressDefaultValues,
+  });
+
+  const [isPending, startTransition] = useTransition();
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <div className="max-w-md mx-auto space-y-4">
+        <h1 className="h2-bold mt-4">Shipping Address</h1>
+        <p className="text-sm text-muted-foreground">
+          Please enter and address to ship to
+        </p>
+        <Form {...form}>
+          <form
+            method="post"
+            className="space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col gap-5">
+              <FormField
+                control={form.control}
+                name="fullName"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<ShippingAddress, "fullName">;
+                }) => (
+                  <FormItem>
+                    <FormLabel>Full Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter full name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <FormField
+                control={form.control}
+                name="streetAddress"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<
+                    ShippingAddress,
+                    "streetAddress"
+                  >;
+                }) => (
+                  <FormItem>
+                    <FormLabel>Address</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter address" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <FormField
+                control={form.control}
+                name="city"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<ShippingAddress, "city">;
+                }) => (
+                  <FormItem>
+                    <FormLabel>City</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter city" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <FormField
+                control={form.control}
+                name="postalCode"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<ShippingAddress, "postalCode">;
+                }) => (
+                  <FormItem>
+                    <FormLabel>Postal Code</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter postal code" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <FormField
+                control={form.control}
+                name="country"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<ShippingAddress, "country">;
+                }) => (
+                  <FormItem>
+                    <FormLabel>Country</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter country" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex gap-2">
+              <Button type="submit" disabled={isPending}>
+                {isPending ? (
+                  <Loader className="size-4 animate-spin" />
+                ) : (
+                  <ArrowRight className="size-4" />
+                )}{" "}
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default ShippingAddressForm;
