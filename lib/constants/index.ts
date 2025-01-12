79d63ea379bb9d5afced5e1f386d2948128084f5
Uncle <1215661954@qq.com>
2025-01-13 01:39:45 +0800

    56 -Shipping Address Zod Schema & Page

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,5 @@
+import { ShippingAddress } from "@/types";
+
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
@@ -6,3 +8,11 @@ export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
+
+export const shippingAddressDefaultValues: ShippingAddress = {
+  fullName: "Jhon Doe",
+  streetAddress: "123 Main St",
+  city: "Anytown",
+  postalCode: "12345",
+  country: "USA",
+};
