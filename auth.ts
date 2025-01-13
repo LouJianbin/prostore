efd6bce68ff901be4db0975c09edf01affc7b3d9
Uncle <1215661954@qq.com>
2025-01-13 21:56:19 +0800

    60 -Persist Session Cart

--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import { prisma } from "./db/prisma";
 import type { Provider } from "next-auth/providers";
 import { compareSync } from "bcrypt-ts-edge";
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 const providers: Provider[] = [
   Credentials({
@@ -57,9 +58,10 @@ const config: NextAuthConfig = {
   },
   callbacks: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    async jwt({ token, user }: any) {
+    async jwt({ token, user, trigger }: any) {
       // Assign user fields to token
       if (user) {
+        token.id = user.id;
         token.role = user.role;
 
         // If user has no name then use the email
@@ -71,6 +73,28 @@ const config: NextAuthConfig = {
             where: { id: user.id },
             data: { name: token.name },
           });
+        }
+
+        if (trigger === "signIn" || trigger === "signUp") {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({ where: { userId: user.id } });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
         }
       }
       return token;
