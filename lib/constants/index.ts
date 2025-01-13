87cf1d06869c2f0ae51e4b71c265ea9830869c22
Uncle <1215661954@qq.com>
2025-01-14 00:11:05 +0800

    64 -Payment Method Page

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -20,5 +20,5 @@ export const shippingAddressDefaultValues: ShippingAddress = {
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   ? process.env.PAYMENT_METHODS.split(",")
   : ["PayPal", "Stripe", "CashOnDelivery"];
-export const PAYMENT_METHODS_DEFAULT =
-  process.env.PAYMENT_METHODS_DEFAULT || "PayPal";
+export const DEFAULT_PAYMENT_METHOD =
+  process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
