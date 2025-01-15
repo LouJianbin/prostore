773d065fee6a27a9d67967b83b99edce06de6ae7
Uncle <1215661954@qq.com>
2025-01-15 14:49:50 +0800

    78 -Create Order & Capture Payment Request

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,6 +1,39 @@
 const base = process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com/";
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+        // "PayPal-Request-Id": "7b92603e-77ed-4896-8e78-5dea2050476a",
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [{ amount: { currency_code: "USD", value: price } }],
+      }),
+    });
+
+    return handleResponese(response);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return handleResponese(response);
+  },
+};
 
 // Generate paypal access token
 async function generateAccessToken() {
@@ -18,9 +51,13 @@ async function generateAccessToken() {
     },
   });
 
+  const jsonData = await handleResponese(response);
+  return jsonData.access_token;
+}
+
+async function handleResponese(response: Response) {
   if (response.ok) {
-    const jsonData = await response.json();
-    return jsonData.access_token;
+    return response.json();
   } else {
     const errorMeassage = await response.text();
     throw new Error(errorMeassage);
