1130f184d3b34c7c70347e2f8d751c88a8ca0bfe
Uncle <1215661954@qq.com>
2025-01-18 23:46:53 +0800

    85 -Get My Orders Action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -11,6 +11,7 @@ import { CartItem, PaymentResult } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import alipay from "../alipay";
+import { PAGE_SIZE } from "../constants";
 
 // Create order and create the order items
 export async function createOrder() {
@@ -335,4 +336,29 @@ async function updateOrderToPaid({
   });
 
   if (!updatedOrder) throw new Error("Order not found");
+}
+
+// Get user's orders
+export async function getMyorders({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const session = await auth();
+  if (!session) throw new Error("User is not authorized");
+
+  const data = await prisma.order.findMany({
+    where: { userId: session.user?.id },
+    orderBy: { createdAt: "desc" },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.order.count({
+    where: { userId: session.user?.id },
+  });
+
+  return { data, totalPages: Math.ceil(dataCount / limit) };
 }
